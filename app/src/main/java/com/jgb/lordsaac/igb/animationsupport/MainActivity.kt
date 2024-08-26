8770cda8f4c70cf4e51d6291779b1bae2dc85cee
José I. Gutiérrez B. <jose19.26@hotmail.com>
2024-08-26 14:32:47 -0500

    Update MainActivity.kt

--- app/src/main/java/com/jgb/lordsaac/igb/animationsupport/MainActivity.kt
+++ app/src/main/java/com/jgb/lordsaac/igb/animationsupport/MainActivity.kt
@@ -5,28 +5,33 @@ import android.os.Bundle
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 import com.jgb.lordsaac.igb.supportanimation.*
+import com.jgb.lordsaac.igb.supportanimation.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //in onCreate() replace setContentView() with:
 
-        // var img = findViewById(R.id.imageView)
     }
 
     fun clickButton (v: View){
         // Add textView for effect vibrate
-
-        // AnimationTextView.animateVibrationTV(tv_hello,this)
-       // AnimationTextView.animateVibrationTV(tv_hello2,this)
+        val recycler_view =  findViewById(R.id.tv_hello) as TextView
+        AnimationTextView.animateVibrationTV(recycler_view,this)
+        AnimationTextView.animateVibrationTV(recycler_view,this)
     }
 
 
     fun clickButton2(v: View){
+        val recycler_view =  findViewById(R.id.tv_hello) as TextView
         // Add view for effect out
-       // AnimationView.animationOut(content_view,this)
+        AnimationView.animationOut(recycler_view,this)
     }
 }
